0833c259386b320358d48cf923509d39dd20bbcf
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-06-13 19:25:55 +0200

    13062024

---
+++ contacts.js
@@ -0,0 +1,28 @@
+// оголошую змінну, яка використовує модуль fs(система файлів).
+// метод .promises дозволяє використовувати обіцянки для асинхронних операцій з файлами
+const fs = require("fs").promises;
+
+// треба для роботи з шляхами файлів
+const path = require("path").promises;
+
+/*
+ * Skomentuj i zapisz wartość
+ * const contactsPath = ;
+ */
+
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+  // ...twój kod
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}
